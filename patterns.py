c4d8501515bfb840e673266b094de07e9ccb94f8
anthonysokolov <anthonysokolov20@gmail.com>
2020-08-31 00:08:01 -0400

    updated fibonacci

--- patterns.py
+++ patterns.py
@@ -7,12 +7,12 @@ def ascending(num=7, start=0, step=1):
 def descending(num=7, start=127, step=1):
     return [mido.Message("note_on", note = start - i*step)for i in range(num)]
 
-def randAsc(num=7, start=0):
+def randAsc(num=7, start=0, vel=8):
     out = []
     current = start
     for i in range(num):
         out.append(mido.Message("note_on", note = current))
-        current = current + randint(1,9)
+        current = current + randint(0,vel)
 
     return out
 
--- songs/song2.py
+++ songs/song2.py
@@ -16,21 +16,26 @@ import patterns as p
 port = "minilogue SOUND"
 outport = mido.open_output(port)
 
-# generate messages
-msg = mido.Message("note_on",note=57)
 
 
-for note in range(57,71,2):
-    msg = mido.Message("note_on",note=57)
-    i = 0.001
-    j = 0.001
+for loop in range(3):
+    # generate messages
+    #msg = mido.Message("note_on",note=57 + 2*loop)
+    size = 6
+    messages = p.randAsc(size,start=57+loop,vel=3)
 
-    for x in range(13):
-        outport.send(msg)
-        time.sleep(i+j)
+    for y in range(7,11):
+        i = 0.01
+        j = 0.001
 
-        temp = i
-        i = i+j
-        j = temp
+        for x in range(y):
+            msg = messages[x % size]
+            outport.send(msg)
+            time.sleep(i+j)
 
-    time.sleep(.3)
+            temp = i
+            i = i+j
+            j = temp
+
+        time.sleep(1/y)
+    time.sleep(.35)
